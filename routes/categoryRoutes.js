96f85446d9a6c9df5dfca3d093feaf502557698e
rahe12 <144845749+rahe12@users.noreply.github.com>
2025-05-21 15:06:33 +0200

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const categoryController = require('../controllers/categoryController');
-
+const authenticateToken = require('../middleware/authMiddleware'); // Import the middleware
 const router = express.Router();
 
+// Apply middleware to protect all routes
+router.use(authenticateToken);
+
+// Then define your routes as before
 router.get('/', categoryController.getCategories);
 router.post('/', categoryController.createCategory);
 router.put('/:id', categoryController.updateCategory);

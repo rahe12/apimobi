02c8fc94abb1035871c216c560ce4bf39eb133a3
rahe12 <144845749+rahe12@users.noreply.github.com>
2025-06-09 12:47:42 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authController = require('../controllers/authController');
-const authenticateToken = require('../middleware/auth');
+const authenticateToken = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/register', authController.register);
